47797646b746039959486cde94d0bb41a5ab075d
Shohel Rana <sohel.rana@netmediamobile.com>
2024-09-17 13:48:03 +0600

    Faculty and Staff api added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,34 +8,70 @@ use App\User;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
-    public function getUserFromApi(){
-            $client = new Client();
-         $response = $client->request('GET','https://api.bup.edu.bd/api/GetOfficeEmployeeProfile?officeId=0&page=0&size=0');
-         // return $response->getBody();
-        $json_reponse_data = json_decode($response->getBody(),true);
-        foreach ($json_reponse_data as $key =>  $employee) {
+    private function create_or_update_employee($data_val){
+        foreach ($data_val as $key =>  $employee) {
             // return $employee; die();
             // return $employee['BupNo'];die;
             if ($employee['Email'] == 'N/A'){
                 $employee['Email'] = $employee['BupNo'];
             }
-            $employee = User::updateOrCreate(
-                [
-                    'bup_id' =>  $employee['BupNo'],
-                    'name' =>  $employee['NameEng'],
-                    'designation_id' =>  $employee['Designation'],
-                    'department_id' =>  $employee['Office'],
-                    'emp_type_id' =>  $employee['AppointmentType'],
-                    'email' =>  $employee['Email'],
-                    'mobile' =>  $employee['Mobile'],
-                    'photo' =>  $employee['PhotoPath'],
-                    'rank' =>  $employee['Rank'],
-                    'blood_group' =>  $employee['BloodGroup'],
-                    'password' => Hash::make($employee['BupNo'])
-                ]
-            );
-            $employee->assignRole('GeneralUser');
+            if (isset($employee['Office'])){
+                $employee['Office'] = $employee['Office'];
+            }
+            else{
+                $employee['Office'] = $employee['Department'];
+            }
+            $employee_get = User::where('email',  $employee['Email'])->first();
+            if ($employee_get){
+                $employee_get->bup_id=$employee['BupNo'];
+                $employee_get->name=$employee['NameEng'];
+                $employee_get->designation_id=$employee['Designation'];
+                $employee_get->department_id=$employee['Office'];
+                $employee_get->emp_type_id=$employee['AppointmentType'];
+                $employee_get->mobile=$employee['Mobile'];
+                $employee_get->photo=$employee['PhotoPath'];
+                $employee_get->rank=$employee['Rank'];
+                $employee_get->blood_group=$employee['BloodGroup'];
+                $employee_get->save();
+            }
+            else{
+                $employee_create = User::create(
+                    [
+                        'bup_id' =>  $employee['BupNo'],
+                        'name' =>  $employee['NameEng'],
+                        'designation_id' =>  $employee['Designation'],
+                        'department_id' =>  $employee['Office'],
+                        'emp_type_id' =>  $employee['AppointmentType'],
+                        'email' =>  $employee['Email'],
+                        'mobile' =>  $employee['Mobile'],
+                        'photo' =>  $employee['PhotoPath'],
+                        'rank' =>  $employee['Rank'],
+                        'blood_group' =>  $employee['BloodGroup'],
+                        'password' => Hash::make($employee['BupNo'])
+                    ]
+                );
+                $employee_create->assignRole('GeneralUser');
+            }
          
         }
+    }
+    public function getUserFromApi(){
+         $client = new Client();
+         // Staff API
+         $response = $client->request('GET','https://api.bup.edu.bd/api/GetOfficeEmployeeProfile?officeId=0&page=0&size=0');
+         // return $response->getBody();
+        $json_reponse_data = json_decode($response->getBody(),true);
+        $this->create_or_update_employee($json_reponse_data);
+        // Faculty Staff API
+         $f_staff_response = $client->request('GET','https://api.bup.edu.bd/api/GetFacultyDepartmentWiseStaffProfile?departmentId=0&facultyId=0');
+         // return $response->getBody();
+        $f_staff_reponse_json = json_decode($f_staff_response->getBody(),true);
+        $this->create_or_update_employee($f_staff_reponse_json);
+        // Faculty/Teacher API
+         $f_staff_response = $client->request('GET','https://api.bup.edu.bd/api/GetFacultyDepartmentWiseProfile?departmentId=0&facultyId=0');
+         // return $response->getBody();
+        $f_staff_reponse_json = json_decode($f_staff_response->getBody(),true);
+        $this->create_or_update_employee($f_staff_reponse_json);
+        
     }
 }
